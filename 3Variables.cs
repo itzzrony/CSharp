214392144bca21ba73fe76c04c8c8e046d5d3167
itzzrony <132839937+itzzrony@users.noreply.github.com>
2023-05-20 23:30:26 +0600

    Create 3Variables.cs
    Different variables

---
+++ 3Variables.cs
@@ -0,0 +1,13 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string ok = "Hello"; // we can put string values but need that on quotation mark without that the value wont work.
+        int var = 10; // we can put the integar full value in here. N.B: we can not put point value in here
+        double var2 = 10.12; //we can put two values after point 
+        float var3 = 1.1; // we can put just one number after point
+        bool var4 = true; // we can set 0 or 1 value with true and false. true means 1 and false means 0
+
+        Console.ReadLine(); // for holding the console for see the output
+    }
+}
