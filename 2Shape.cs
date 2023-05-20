56b56ef6da4cf9cd0cc6b7c6c1d722cbac9be1a7
itzzrony <132839937+itzzrony@users.noreply.github.com>
2023-05-20 23:24:37 +0600

    Create 2Shape.cs
    Drawing a shape using the console message

---
+++ 2Shape.cs
@@ -0,0 +1,14 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("      /|"); // Drawing a shape by using console message
+        Console.WriteLine("     / |");
+        Console.WriteLine("    /  |");
+        Console.WriteLine("   /   |");
+        Console.WriteLine("  /    |");
+        Console.WriteLine(" /_____|");
+
+        Console.ReadLine(); // for holding the console for see the output
+    }
+}
