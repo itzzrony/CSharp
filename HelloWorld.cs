ba48457b22c892957f1a17b76cb9083782658da0
itzzrony <132839937+itzzrony@users.noreply.github.com>
2023-05-20 23:15:01 +0600

    Create HelloWorld.cs
    Simply output the hello world message! This is our first program.

---
+++ HelloWorld.cs
@@ -0,0 +1,9 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!"); // this is for printing the message, and in the "" this is the message string value
+
+        Console.ReadLine(); // for holding the console for see the output
+    }
+}
